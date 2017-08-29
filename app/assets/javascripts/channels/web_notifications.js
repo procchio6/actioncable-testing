f2871da54d74ee6c7dc6eecb4aa36acd56b94136
Pat Rocchio <procchio6@gmail.com>
2017-08-29 17:42:14 -0400

    Play with params

--- app/assets/javascripts/channels/web_notifications.js
+++ app/assets/javascripts/channels/web_notifications.js
@@ -1,11 +1,15 @@
-App.web_notifications = App.cable.subscriptions.create("WebNotificationsChannel",
+App.web_notifications = App.cable.subscriptions.create(
+  {
+    channel: "WebNotificationsChannel",
+    room: "Best Room"
+  },
   {
     connected: function (data) {
       App.web_notifications.send({message: '<p>Someone connected!</p>'})
     },
 
     disconnected: function () {
-      
+
     },
 
     received: function (data) {
--- app/channels/web_notifications_channel.rb
+++ app/channels/web_notifications_channel.rb
@@ -1,13 +1,13 @@
 class WebNotificationsChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "web_notifications_channel"
+    stream_from "web_notifications_channel_#{params[:room]}"
   end
 
   def receive(data)
-    ActionCable.server.broadcast("web_notifications_channel", data)
+    ActionCable.server.broadcast("web_notifications_channel_#{params[:room]}", data)
   end
 
   def unsubscribed
-    ActionCable.server.broadcast("web_notifications_channel", {message: '<p>Someone left!</p>'})
+    ActionCable.server.broadcast("web_notifications_channel_#{params[:room]}", {message: '<p>Someone left!</p>'})
   end
 end
